7f1a7403944758775164a69b43dd155dfe01bb10
Vinh Le <vinh@vertics.co>
2019-11-17 09:00:53 +0200

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Vinh Le on 11/17/19.
+//  Copyright © 2019 Vinh Le. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
