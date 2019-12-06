6a3e87795861f521477316af591fc92a60884afe
Vinh Le <vinh@vertics.co>
2019-12-06 08:11:41 +0200

    feat: add Firebase

--- Instagram/ViewController.swift
+++ Instagram/ViewController.swift
@@ -9,12 +9,84 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // Plus button
+    let plusPhotoButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "plush_photo"), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    // User name text field
+    let usernameTextField: UITextField = {
+        let textfield = UITextField()
+        textfield.placeholder = "User name"
+        textfield.translatesAutoresizingMaskIntoConstraints = false
+        textfield.borderStyle = .roundedRect
+        textfield.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        
+        return textfield
+    }()
+    
+    // Email text field
+    let emailTextField: UITextField = {
+        let textfield = UITextField()
+        textfield.placeholder = "Enter your email"
+        textfield.translatesAutoresizingMaskIntoConstraints = false
+        textfield.borderStyle = .roundedRect
+        textfield.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        
+        return textfield
+    }()
+    
+    // Password text field
+    let passwordTextField: UITextField = {
+        let textfield = UITextField()
+        textfield.placeholder = "Password"
+        textfield.isSecureTextEntry = true
+        textfield.translatesAutoresizingMaskIntoConstraints = false
+        textfield.borderStyle = .roundedRect
+        textfield.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        
+        return textfield
+    }()
+    
+    // Sign up button
+    let signupButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Sign up", for: .normal)
+        button.backgroundColor = UIColor.rgb(red: 149, green: 204, blue: 244)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.cornerRadius = 4
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(plusPhotoButton)
+        
+        plusPhotoButton.anchor(top: view.topAnchor, bottom: nil, left: nil, right: nil, paddingTop: 40, paddingBottom: 40, paddingLeft: 0, paddingRight: 0, width: 140, height: 140)
+        plusPhotoButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    
+        // Init input text fields
+        setInputFields()
     }
-
-
+    
+    fileprivate func setInputFields() {
+        // create a stack view
+        let stackView = UIStackView(arrangedSubviews: [
+            usernameTextField, emailTextField, passwordTextField, signupButton
+        ])
+    
+        stackView.distribution = .fillEqually
+        stackView.axis = .vertical
+        stackView.spacing = 10
+        
+        // Add Stack view to view
+        view.addSubview(stackView)
+        
+        // Add constraints
+        stackView.anchor(top: plusPhotoButton.bottomAnchor, bottom: nil, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 40, paddingBottom: 0, paddingLeft: 20, paddingRight: 20, width: 0, height: 200)
+    }
 }
-
