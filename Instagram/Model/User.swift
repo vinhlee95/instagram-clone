86fb80c0013d4545ec1d4b1cff24a85190705f16
Vinh Le <vinh@vertics.co>
2019-12-14 08:51:14 +0200

    feat: create user model

---
+++ Instagram/Model/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  Instagram
+//
+//  Created by Vinh Le on 12/14/19.
+//  Copyright © 2019 Vinh Le. All rights reserved.
+//
+struct User {
+    let name: String
+    let profileImageUrl: String
+    
+    init(name: String, profileImageUrl: String) {
+        self.name = name
+        self.profileImageUrl = profileImageUrl
+    }
+}
