8ee0ddbce688fecfd1f423ad4a65d983fc8c9549
Vinh Le <vinh@vertics.co>
2019-12-24 21:14:10 +0200

    feat: init login view with signup button * lead user to sign up view when user clicks sign up button

--- Instagram/View Controllers/Login/LoginController.swift
+++ Instagram/View Controllers/Login/LoginController.swift
@@ -9,9 +9,37 @@
 import UIKit
 
 class LoginController: UIViewController {
+    
+    let signupButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Don't have an account? Sign up", for: .normal)
+        button.addTarget(self, action: #selector(onSignupClick), for: .touchUpInside)
+        return button
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.view.backgroundColor = .red
+        navigationController?.isNavigationBarHidden = true
+        
+        self.view.backgroundColor = .white
+        renderSignupButton()
+    }
+    
+    //
+    // Render Sign up button
+    //
+    fileprivate func renderSignupButton() {
+        self.view.addSubview(signupButton)
+        signupButton.anchor(top: nil, bottom: view.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 0, paddingBottom: 0, paddingLeft: 0, paddingRight: 0, width: nil, height: 50)
+    }
+    
+    //
+    // Handle sign up
+    //
+    @objc func onSignupClick() {
+        let signupController = SignupController()
+        navigationController?.pushViewController(signupController, animated: true)
     }
 }
