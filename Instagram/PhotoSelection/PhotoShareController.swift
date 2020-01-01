63ac8e19bb816b186dfa942f01540fc097ab85c7
Vinh Le <vinh@vertics.co>
2020-01-01 09:26:04 +0200

    feat: add post sharing functionality * save image data to Firebase storage and get download url * save download url to Posts collection on Firebase database * dismiss sharing view in completion and handle button states accordingly

--- Instagram/PhotoSelection/PhotoShareController.swift
+++ Instagram/PhotoSelection/PhotoShareController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class PhotoShareController: UIViewController {
     //
@@ -22,6 +23,7 @@ class PhotoShareController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = UIColor.rgb(red: 240, green: 240, blue: 240)
         renderHeaderView()
+        renderRightNavigationButton()
     }
     
     override var prefersStatusBarHidden: Bool {
@@ -47,6 +49,14 @@ class PhotoShareController: UIViewController {
         return tv
     }()
     
+    //
+    // Right navigation button
+    //
+    fileprivate func renderRightNavigationButton() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Share", style: .plain
+            , target: self, action: #selector(onShareClick))
+    }
+    
     fileprivate func renderHeaderView() {
         let headerView = UIView()
         view.addSubview(headerView)
@@ -59,5 +69,88 @@ class PhotoShareController: UIViewController {
         
         headerView.addSubview(textView)
         textView.anchor(top: headerView.topAnchor, bottom: headerView.bottomAnchor, left: imageView.rightAnchor, right: headerView.rightAnchor, paddingTop: 0, paddingBottom: 8, paddingLeft: 0, paddingRight: 0, width: nil, height: nil)
+    }
+}
+
+//
+// Handle sharing photo:
+// * Upload image as data to Firebase Storage
+// * Get download url of the image
+// * Save download url to Firebase Database with pointer to user uid
+//
+extension PhotoShareController {
+    @objc func onShareClick() {
+        uploadImage { (url) in
+            self.saveImageUrl(url, completion: {
+                self.dismiss(animated: true, completion: nil)
+            })
+        }
+    }
+    
+    //
+    // Upload image to Firebase Storage
+    //
+    fileprivate func uploadImage(completion: @escaping (_ url: String) -> Void) {
+        guard let image = self.imageView.image else {return}
+        
+        let storageRef = Storage.storage().reference()
+        let filename = NSUUID().uuidString
+        let sharingPhotoRef = storageRef.child("post/\(filename)")
+        guard let  uploadData = image.jpegData(compressionQuality: 0.5) else {return}
+        
+        disableShareButton()
+        
+        sharingPhotoRef.putData(uploadData, metadata: nil) { (metadata, error) in
+            if let error = error {
+                print("Error in uploading image to Firebase", error)
+                self.enableShareButton()
+                return
+            }
+            
+            sharingPhotoRef.downloadURL { (downloadUrl, error) in
+                if let error = error {
+                    print("Error in downloading url", error)
+                    self.enableShareButton()
+                    return
+                }
+                
+                guard let url = downloadUrl?.absoluteString else {return}
+                
+                completion(url)
+            }
+        }
+    }
+    
+    //
+    // Save image url to Firebase database
+    //
+    fileprivate func saveImageUrl(_ url: String, completion: @escaping () -> Void) {
+        guard let userId = Auth.auth().currentUser?.uid else {return}
+        let userPostRef = Database.database().reference().child("posts").child(userId)
+        let ref = userPostRef.childByAutoId()
+        
+        let values = ["imageUrl": url, "caption": self.textView.text ?? ""]
+        ref.updateChildValues(values) { (error, dbRef) in
+            if let error = error {
+                print("Error in saving image url to posts collection on db", error)
+                return
+            }
+            
+            print("Successfully save image url to db")
+            completion()
+        }
+    }
+}
+
+//
+// Enable and Disable Share button based on current uploading and saving process
+//
+extension PhotoShareController {
+    fileprivate func disableShareButton() {
+        navigationItem.rightBarButtonItem?.isEnabled = false
+    }
+    
+    fileprivate func enableShareButton() {
+        navigationItem.rightBarButtonItem?.isEnabled = true
     }
 }
