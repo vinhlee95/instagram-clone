8ac91d12ab679d1ab65966b252b0e532660b1fd5
Vinh Le <vinh@vertics.co>
2020-02-16 07:42:36 +0200

    refactor anchoring logics of home cell elements

--- Instagram/View Controllers/Home/HomeCell.swift
+++ Instagram/View Controllers/Home/HomeCell.swift
@@ -42,32 +42,48 @@ class HomeCell: UICollectionViewCell {
         return label
     }()
     
+    let optionButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("•••", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        return button
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        renderUserProfilePhoto()
-        renderUsernameLabel()
-        renderPostPhoto()
+        // Add sub-views first to ensure following subviews have the same parent
+        // https://stackoverflow.com/questions/35452908/swift-add-constraint-programmatically
+        addSubview(userprofileImageView)
+        addSubview(usernameLabelView)
+        addSubview(photoImageView)
+        addSubview(optionButton)
+        
+        anchorUserProfilePhoto()
+        anchorUsernameLabel()
+        anchorPostPhoto()
+        anchorOptionButton()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    fileprivate func renderUserProfilePhoto() {
-        addSubview(userprofileImageView)
+    fileprivate func anchorUserProfilePhoto() {
         userprofileImageView.anchor(top: topAnchor, bottom: nil, left: leftAnchor, right: nil, paddingTop: 8, paddingBottom: 8, paddingLeft: 8, paddingRight: 0, width: 40, height: 40)
         userprofileImageView.layer.cornerRadius = 40/2
     }
     
-    fileprivate func renderUsernameLabel() {
-        addSubview(usernameLabelView)
-        usernameLabelView.anchor(top: topAnchor, bottom: nil, left: userprofileImageView.rightAnchor, right: rightAnchor, paddingTop: 18, paddingBottom: 0, paddingLeft: 8, paddingRight: 0, width: nil, height: nil)
+    fileprivate func anchorUsernameLabel() {
+        usernameLabelView.anchor(top: topAnchor, bottom: photoImageView.topAnchor, left: userprofileImageView.rightAnchor, right: optionButton.leftAnchor, paddingTop: 0, paddingBottom: 0, paddingLeft: 8, paddingRight: 0, width: nil, height: nil)
     }
     
-    fileprivate func renderPostPhoto() {
-        addSubview(photoImageView)
+    fileprivate func anchorPostPhoto() {
         photoImageView.anchor(top: userprofileImageView.bottomAnchor, bottom: bottomAnchor, left: leftAnchor, right: rightAnchor, paddingTop: 8, paddingBottom: 0, paddingLeft: 0, paddingRight: 0, width: nil, height: nil)
+    }
+    
+    fileprivate func anchorOptionButton() {
+        optionButton.anchor(top: topAnchor, bottom: photoImageView.topAnchor, left: nil, right: rightAnchor, paddingTop: 0, paddingBottom: 0, paddingLeft: 0, paddingRight: 8, width: nil, height: nil)
     }
 }
 
