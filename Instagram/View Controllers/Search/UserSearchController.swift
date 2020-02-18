e0a275248e84a33ef256e957845aeb3fe0873c2b
Vinh Le <vinh@vertics.co>
2020-02-18 06:49:22 +0200

    feat: init search bar

--- Instagram/View Controllers/Search/UserSearchController.swift
+++ Instagram/View Controllers/Search/UserSearchController.swift
@@ -9,8 +9,19 @@
 import UIKit
 
 class UserSearchController: UICollectionViewController {
+    
+    let searchBar: UISearchBar = {
+        let sb = UISearchBar()
+        sb.placeholder = "Type user name"
+        return sb
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        collectionView.backgroundColor = .red
+        collectionView.backgroundColor = .white
+        
+        let navBar = navigationController?.navigationBar
+        navBar?.addSubview(searchBar)
+        searchBar.anchor(top: navBar?.topAnchor, bottom: navBar?.bottomAnchor, left: navBar?.leftAnchor, right: navBar?.rightAnchor, paddingTop: 0, paddingBottom: 0, paddingLeft: 8, paddingRight: 8, width: nil, height: nil)
     }
 }
