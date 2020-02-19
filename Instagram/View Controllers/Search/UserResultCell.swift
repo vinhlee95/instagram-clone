f34bcd925396197562dfdd0c84d57a068113dc33
Vinh Le <vinh@vertics.co>
2020-02-19 06:37:36 +0200

    feat: init search view with cells

---
+++ Instagram/View Controllers/Search/UserResultCell.swift
@@ -0,0 +1,20 @@
+//
+//  UserResultCell.swift
+//  Instagram
+//
+//  Created by Vinh Le on 2/19/20.
+//  Copyright © 2020 Vinh Le. All rights reserved.
+//
+
+import UIKit
+
+class UserResultCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = .blue
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- Instagram/View Controllers/Search/UserSearchController.swift
+++ Instagram/View Controllers/Search/UserSearchController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class UserSearchController: UICollectionViewController {
+    //
+    // Variables
+    //
+    private var cellId = "cellId"
     
     let searchBar: UISearchBar = {
         let sb = UISearchBar()
@@ -23,5 +27,35 @@ class UserSearchController: UICollectionViewController {
         let navBar = navigationController?.navigationBar
         navBar?.addSubview(searchBar)
         searchBar.anchor(top: navBar?.topAnchor, bottom: navBar?.bottomAnchor, left: navBar?.leftAnchor, right: navBar?.rightAnchor, paddingTop: 0, paddingBottom: 0, paddingLeft: 8, paddingRight: 8, width: nil, height: nil)
+        
+        // Register cell
+        collectionView.register(UserResultCell.self, forCellWithReuseIdentifier: cellId)
+    }
+}
+
+//
+// Configure cells
+//
+extension UserSearchController {
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        cell.backgroundColor = .red
+        return cell
+    }
+}
+
+//
+// Configure sizing for cells
+//
+extension UserSearchController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = view.frame.width
+        let height = CGFloat(50)
+        
+        return CGSize(width: width, height: height)
     }
 }
