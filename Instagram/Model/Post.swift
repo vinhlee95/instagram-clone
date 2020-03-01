626e5af795ebbfc0bfb2a4c2e07256ea27da3ac6
Vinh Le <vinh@vertics.co>
2020-03-01 10:40:05 +0200

    refactor: change creationDate in post dictionary to timeIntervalSince1970

--- Instagram/Model/Post.swift
+++ Instagram/Model/Post.swift
@@ -31,6 +31,6 @@ struct Post {
 
 extension Post {
     var postDictionary: [String: Any] {
-        return ["imageUrl": imageUrl, "caption": caption, "imageWidth": imageWidth, "imageHeight": imageHeight, "creationDate": creationDate]
+        return ["imageUrl": imageUrl, "caption": caption, "imageWidth": imageWidth, "imageHeight": imageHeight, "creationDate": creationDate.timeIntervalSince1970]
     }
 }
